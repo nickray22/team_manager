0ba6d37797f1c2c122301376251bd5cd1cea42c7
Ian Gawronski <iangawronski@gmail.com>
2015-03-07 12:03:11 -0500

    updated registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,32 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
 
-  def choose
-    @manager = Manager.new(manager_params)
-    @manager.save!
-    if @manager.save
-      render json: { :manager => @manager }, status: :created
-    else
-      render json: { messages: @manager.errors.full_messages}, status: :unprocessable_entity
-    end
-  end
-
-  private
-    def manager_params
-      params.require(:manager).permit(:user_id, :manager_id, :name, :team_id)
-    end
-
-    def parent_params
-      params.require(:parent).permit(:player_id, :parent_id, :name, :user_id)
-    end
-
-    # def player_params
-    #   params.require(:player).permit(:roster_id, :user_id, :gender, :age, :name, :player_id)
-    # end
-
-    def coach_params
-      params.require(:coach).permit(:coach_id, :team_id, :league_id, :user_id, :name)
-    end
 end
 
 
